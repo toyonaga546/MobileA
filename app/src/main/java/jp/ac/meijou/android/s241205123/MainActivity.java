716d70a5ce19cb0f48e6e4f0f3e667ea9abdb232
豊永 候士朗 <241205123@ie.meijo-u.ac.jp>
2025-09-02 13:30:12 +0900

    テキスト編集の演習

--- app/src/main/java/jp/ac/meijou/android/s241205123/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s241205123/MainActivity.java
@@ -1,6 +1,9 @@
 package jp.ac.meijou.android.s241205123;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,54 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205123.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        //ここから
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //ここまで
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.text.setText(R.string.text);
+
+        binding.button.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+
+            binding.text.setText(text);
+        });
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // テキストが更新される直前に呼ばれる
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // 文字を1つ入力された時に呼ばれる
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // テキストが更新されたあとに呼ばれる
+                binding.text.setText(editable.toString());
+            }
+        });
+
+
+
+
     }
 }
